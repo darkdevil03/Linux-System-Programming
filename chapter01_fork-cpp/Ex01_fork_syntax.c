78891642caf198e81abc4d5f91e53ba12bff330b
xap <bvvsatyanarayana280301@gmail.com>
2026-07-24 14:24:22 +0530

    For Ex01_fork_syntax.c, Implemented to create a child process using fork() of system call function.

--- chapter01_fork-cpp/Ex01_fork_syntax.c
+++ chapter01_fork-cpp/Ex01_fork_syntax.c
@@ -7,7 +7,7 @@ int main() {
     int pid = fork(); // creating process inside a process(Ex01_fork_syntax.c)
 
     if (pid == 0) {
-        printf("This is child process\n");
+        printf("This is child process!\n");
     }
     else {
         printf("This is Parent process\n");
