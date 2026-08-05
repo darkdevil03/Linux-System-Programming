8084eb65c06cb6c83e4ef7b8ce25b3988fedef54
xap <bvvsatyanarayana280301@gmail.com>
2026-08-05 16:35:59 +0530

    For Ex01_command_line_argument.c, Added the how to give commandline arguments via terminal.

--- chapter00_basics-of-c/Ex01_command_line_argument.c
+++ chapter00_basics-of-c/Ex01_command_line_argument.c
@@ -1,3 +1,8 @@
+/**
+    To use command-line arguments,
+        We need to modify your main function to accept argc (argument count) and argv (argument vector/values).
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
@@ -11,3 +16,23 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+/*
+     In terminal go to this program directory then:
+        -> syntax : gcc_compiler program_name.c
+
+         $ gcc Ex01_command_line_argument.c
+
+         -> ./a.out arguments
+         $ ./a.out ram dev
+
+    sample output:
+            Total arguments provided: 3
+
+            Argument 0: ./a.out
+            Argument 1: ram
+            Argument 2: dev
+
+
+        Note: The first argument (argv[0]) will always be the name/path of the executable itself (e.g., ./my_project),
+              and the subsequent arguments will be the words you typed after it.
+ */
