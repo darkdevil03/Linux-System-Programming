aff2a6d87d43366db8770db60ce1e12a661369fd
xap <bvvsatyanarayana280301@gmail.com>
2026-09-02 12:49:18 +0530

    Implemented custom shell working behaviour.

--- chapter03_exec-c/Scenario02_terminal_shell_command_working.c
+++ chapter03_exec-c/Scenario02_terminal_shell_command_working.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include<unistd.h>
+#include <sys/wait.h>
 
 int main() {
 
-    printf("[TERMINAL] I am running with a process id: %d",getpid());
+    printf("[TERMINAL] I am running with a process id: %d\n",getpid());
 
     while (1) {
-        char command[30];
+        char *command[30];
         pid_t shell = fork();
-
+        printf("shell_%d$",getpid());
         if (shell == 0) {
-            printf("[Shell->%s]")
+            scanf("%p",command);
+            printf("\n[Shell_%d->%s]\n",getpid(),command[0]);
+            execvp(command[0],command);
+
+            exit(5);
+        }else {
+            int status ;
+            pid_t terminated_shell_id = wait(&status);
+            int exit_code = WEXITSTATUS(status);
+            if (WIFEXITED(status) == 0) {
+                printf("[Shell_%d] terminated normally with exit code %d\n",getpid(),exit_code);
+                if (exit_code == 5) {
+                    printf("[Shell_%d] exec member malfunctioned exit code %d\n",getpid(),exit_code);
+                }
+            }else {
+                printf("[Shell_%d] got signal %d\n",getpid(),exit_code);
+            }
         }
     }
 
