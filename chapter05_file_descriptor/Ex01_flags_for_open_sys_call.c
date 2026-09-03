84093ed53effd139c40747eceffdebbe1e554e6b
xap <bvvsatyanarayana280301@gmail.com>
2026-09-03 12:47:12 +0530

    Implemented open sys_call with flag for if the file not exist create new file with name passed to it.

---
+++ chapter05_file_descriptor/Ex01_flags_for_open_sys_call.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <stdio.h>
+
+int main() {
+
+    // O_CREAT
+    int fd_append_flag_index = open("New_File_Created.txt", O_RDWR | O_CREAT, 0644);
+    // If New_File_Created.txt and this program exist in same directory absolute is optional.
+    // Incase of New_File_Created.txt is available in another folder then we need to absolute path for it.
+    // But the New_File_Created.txt is not available in current directory we use flag O_CREATE for create a .txt with given name(New_File_Created.txt) in this program directory.
+
+    if (fd_append_flag_index == -1) {
+        printf("[ERROR] Failed to creating new file if not exist!!\n");
+    }else {
+        printf("[Success] New_File_Created.txt is created..\n");
+    }
+
+
+    return 0;
+}
