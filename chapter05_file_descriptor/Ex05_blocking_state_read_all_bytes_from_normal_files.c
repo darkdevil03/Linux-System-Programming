7b42e29ef46c4ff83d2fd46e34a2c574c1b47c80
xap <bvvsatyanarayana280301@gmail.com>
2026-09-07 14:22:11 +0530

    For Ex05_blocking_state_read_all_bytes_from_normal_files.c, Added the empty file case condition.

--- chapter05_file_descriptor/Ex05_blocking_state_read_all_bytes_from_normal_files.c
+++ chapter05_file_descriptor/Ex05_blocking_state_read_all_bytes_from_normal_files.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 int main() {
-    int fd = open("File00_test_file.txt", O_RDONLY);
+    int fd = open("/home/xap/Clion/Linux-System-Programming/chapter05_file_descriptor/File00_test_file.txt", O_RDONLY);
 
     if (fd == -1) {
         perror("[ERROR] File descriptor failed to open");
@@ -35,6 +35,10 @@ int main() {
 
         // Advance the tracking pointer, leaving the base 'buf' intact
         ptr += bytes_read;
+    }
+    if (bytes_read == 0) {
+        printf("\nRead file was empty!! Please check the file...\n");
+        return EXIT_SUCCESS;
     }
 
     // Add the null terminator exactly where the reading stopped
