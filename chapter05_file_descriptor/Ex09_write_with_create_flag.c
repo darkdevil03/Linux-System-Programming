c4e63a7aab4aeb13894108b905d0735617502944
xap <bvvsatyanarayana280301@gmail.com>
2026-09-09 15:57:05 +0530

    For Ex09_write_with_create_flag.c respective .txt file: File09_New_Write_File_Created.txt Created After Executing program.

---
+++ chapter05_file_descriptor/Ex09_write_with_create_flag.c
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    int fd_append_flag_index = open("/home/xap/Clion/Linux-System-Programming/chapter05_file_descriptor/File09_New_Write_File_Created.txt", O_WRONLY | O_CREAT | O_APPEND);
+
+    if (fd_append_flag_index == -1) {
+        printf("[ERROR] File not exist!!\n");
+        return EXIT_FAILURE;
+    }
+    printf("[SUCCESS] This program created new .txt file with name: File09_New_Write_File_Created.txt ...\n");
+
+    printf("Enter your text to write into newly created txt file : ");
+    char text_buffer[256];
+    fgets(text_buffer,sizeof(text_buffer),stdin);
+    printf("\n");
+
+    // Here, write overwrite the new data into existing file not append that you wrote.
+    ssize_t byte_written = write(fd_append_flag_index,text_buffer,strlen(text_buffer));
+
+    if (byte_written == -1) {
+        printf("[ERROR] Failed to write data into newly created file ");
+        close(fd_append_flag_index);
+        return EXIT_FAILURE;
+    }
+    printf("[SUCCESS] Written %zd bytes.\n", byte_written);
+
+
+    if (close(fd_append_flag_index) == -1) {
+        printf("[ERROR] Failed To Close File Descriptor");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
