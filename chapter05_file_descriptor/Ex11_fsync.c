76d4798d3f10729d0614974ac493aea7a45475b9
xap <bvvsatyanarayana280301@gmail.com>
2026-09-10 14:47:31 +0530

    For Ex11_fsync.c and Ex12_fdatasync.c, added additional info for printf statements for better understanding.

--- chapter05_file_descriptor/Ex11_fsync.c
+++ chapter05_file_descriptor/Ex11_fsync.c
@@ -5,27 +5,28 @@
 #include <unistd.h>
 
 int main() {
-
-    int fd = open("File00_test_file.txt", O_WRONLY | O_APPEND);
+    char file_path[] = "File00_test_file.txt";
+    int fd = open(file_path, O_WRONLY | O_APPEND);
 
     if (fd == -1) {
-        printf("[ERROR] Failed to open file\n");
+        printf("[ERROR] Failed to open file %s !!\n",file_path);
         return EXIT_FAILURE;
     }
 
-    printf("[SUCCESS] File opened successfully\n");
+    printf("[SUCCESS] File %s opened successfully...\n",file_path);
 
     char write_buffer[] = "Line from Ex11_fsync.c : Fsync related data!";
 
     ssize_t written_bytes = write(fd, write_buffer, strlen(write_buffer));
 
     if (written_bytes == -1) {
-        printf("[ERROR] Failed to write to file\n");
+        printf("[ERROR] Failed to write to file %s !!\n",file_path);
         return EXIT_FAILURE;
     }
     write_buffer[written_bytes] = '\0';
      fsync(fd);
 
+    printf("[SUCCESS] File's data and metadata successfully stored to existing file of {%s} to hard disk.. \n", file_path);
 
     printf("Number of bytes written : %zd\n",written_bytes);
     printf("Written content into file : %s\n",write_buffer);
--- chapter05_file_descriptor/Ex12_fdatasync.c
+++ chapter05_file_descriptor/Ex12_fdatasync.c
@@ -5,32 +5,33 @@
 #include <unistd.h>
 
 int main() {
-
-    int fd = open("File00_test_file.txt", O_WRONLY | O_APPEND);
+    char file_path[] = "File00_test_file.txt";
+    int fd = open(file_path, O_WRONLY | O_APPEND);
 
     if (fd == -1) {
-        printf("[ERROR] Failed to open file\n");
+        printf("[ERROR] Failed to open file %s\n", file_path);
         return EXIT_FAILURE;
     }
 
-    printf("[SUCCESS] File opened successfully\n");
+    printf("[SUCCESS] File %s opened successfully\n",file_path);
 
     char write_buffer[] = "Line from Ex12_fdatasync.c : Fdatasync related data!";
 
     ssize_t written_bytes = write(fd, write_buffer, strlen(write_buffer));
 
     if (written_bytes == -1) {
-        printf("[ERROR] Failed to write to file\n");
+        printf("[ERROR] Failed to write to file %s !!\n",file_path);
         return EXIT_FAILURE;
     }
 
     write_buffer[written_bytes] = '\0';
 
     if (fdatasync(fd) == -1) {
-        printf("[ERROR] Failed to fsync file!\n");
+        printf("[ERROR] Failed to fsync file %s to hard disk!!\n",file_path);
         printf("[Warning]It's now in the control of kernel...\n");
     }
 
+    printf("[SUCCESS] File's data only successfully stored to existing file of {%s} to hard disk.. \n", file_path);
 
     printf("Number of bytes written : %zd\n",written_bytes);
     printf("Written content into file : %s\n",write_buffer);
