06a276b130fe951f62b8d6ec4d33f670cb842b17
xap <bvvsatyanarayana280301@gmail.com>
2026-09-22 12:59:44 +0530

    For Ex30_portable_sleep_with_select.c, added the error validation and removed unused variables.

--- chapter05_file_descriptor/Ex30_portable_sleep_with_select.c
+++ chapter05_file_descriptor/Ex30_portable_sleep_with_select.c
@@ -1,26 +1,49 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/select.h>
 
 int main() {
 
-    fd_set watch_list;
+    printf("[SELECT] Starting the process...\n");
 
+    // 1. Create a timeval structure
     struct timeval timeout;
-    timeout.tv_sec = 15;
-    timeout.tv_usec = 0;
 
-    FD_ZERO(&watch_list);
+    // 2. Set the sleep duration: 15 second and 500,000 microseconds (0.5 seconds)
+    timeout.tv_sec = 15; // Seconds
+    timeout.tv_usec = 5; // Microseconds (1,000,000 usec = 1 sec)
 
-    printf("[SELECT] I am going for sleep for 15sec using select()\n");
+    printf("[MAIN-PROGRAM] I am going for sleep for 15.5sec using select()\n");
+    // 3. THE HACK: Call select(): Capture the return value of select()
+    // - Arg 1: 0 (Highest file descriptor + 1. Since we have none, we pass 0)
+    // - Arg 2: NULL (No files to watch for reading)
+    // - Arg 3: NULL (No files to watch for writing)
+    // - Arg 4: NULL (No files to watch for errors)
+    // - Arg 5: &tv (Our 1.5 second timer)
     int ret = select( 0, nullptr, nullptr, nullptr, &timeout);
 
+    // 4. Error Validation
     if (ret == -1) {
-        printf("[ERROR] Failed to invoke select() !!!\n");
-        return EXIT_FAILURE;
+        if (errno == EINTR) {
+            printf("[WARNING] Sleep was interrupted early by a signal!\n");
+        } else {
+            perror("[ERROR] select() failed due to an invalid argument");
+            return EXIT_FAILURE;
+        }
     }
-
-    printf("[SELECT] Kernel wake-up me  after 15sec!!\n");
+    // 5. Success Validation (Timeout Expired)
+    else if (ret == 0) {
+        // Because we gave select() zero files to watch, returning 0
+        // (meaning 0 files are ready) is the exact successful behavior we want!
+        printf("[SUCCESS] Woke up! The 1.5 second timeout expired normally.\n");
+    }
+    // 6. The "Impossible" Case
+    else {
+        // Since we didn't give it any files to watch, it should be impossible
+        // for select() to find a ready file and return a number greater than 0.
+        printf("[UNKNOWN] select returned %d, which shouldn't happen here!\n", ret);
+    }
 
     return EXIT_SUCCESS;
 }
